a604da496086590f7d63c2b14e17d2ed0161dc31
ghost-mann <iamaob8@protonmail.com>
2025-03-28 18:34:03 +0300

    feat: generated sqlite db containing columns and tables for the weather data

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from src.extract import get_weather_data
-from src.transform import clean_weather_data
-from src.load import save_to_database
+from extract import get_weather_data
+from transform import clean_weather_data
+from load import save_to_database
 
 
 def main():
 
-    API_KEY = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/nairobi?unitGroup=us&key=YOUR_API_KEY&contentType=json'
+    API_KEY = 'aadbb980690277918b13cd7721aa3d06'
     CITY = 'Nairobi'
 
     # Extract
